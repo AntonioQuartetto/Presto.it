f07470010e949aa80fb43264cbe783fee5897c85
Simone Leonardi <simoneleo94@gmail.com>
2023-06-27 20:02:29 +0200

    Quasi finito parte logica form/inserisci annuncio

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -8,6 +8,6 @@ class AnnouncementController extends Controller
 {
     public function create()
     {
-        //
+        return view('announcement.create');
     }
 }
--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -1,3 +1,26 @@
 <div>
-    {{-- Do your work, then step back. --}}
+
+    <h1>Inserisci il tuo annuncio!</h1>
+
+    <form wire:submit.prevent="store" class="mt-4">
+        @csrf
+
+        <div class="form-group">
+          <label for="exampleFormControlInput1">Titolo</label>
+          <input wire:model="title" type="text" class="form-control" placeholder="Inserisci il titolo">
+        </div>
+
+        <div class="form-group mt-3">
+            <label for="exampleFormControlInput1">Prezzo</label>
+            <input wire:model="price" type="number" class="form-control" placeholder="Inserisci il prezzo">
+          </div>
+
+        <div class="form-group mt-4">
+          <label for="exampleFormControlTextarea1">Inserisci descrizione</label>
+          <textarea wire:model="body" type="text" class="form-control" rows="3"></textarea>
+        </div>
+
+        <button type="submit">Inserisci annuncio</button>
+      
+    </form>
 </div>
