2d9422fd425a4d984b271719c9d4b244acf50ca2
Simone Leonardi <simoneleo94@gmail.com>
2023-06-29 17:44:14 +0200

    merge

--- resources/views/page/categoryShow.blade.php
+++ resources/views/page/categoryShow.blade.php
@@ -30,7 +30,7 @@
                     @empty 
                         <div class="col-12">
                             <p class="h1">Non sono presenti Annunci</p>
-                            <p class="h2">Pubblicane uno: <a href="{{route('create-announcement')}}">Inserisci  Annuncio</a></p>
+                            <p class="h2">Pubblicane uno: <a href="{{route('announcement.create')}}">Inserisci  Annuncio</a></p>
                         </div>
 
                     @endforelse
