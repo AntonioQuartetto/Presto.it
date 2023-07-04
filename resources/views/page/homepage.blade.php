ddfd1c084733f52dc802ee494608e2e81fe8ef36
Antonio Quartetto <antonioquartetto98@gmail.com>
2023-07-04 21:56:10 +0200

    aggiunta sezione ultimi annunci

--- resources/views/page/homepage.blade.php
+++ resources/views/page/homepage.blade.php
@@ -1,6 +1,17 @@
 <x-template>
 
-   
+    <div class="container my-3">
+        <div class="text-center mb-2">
+            <h2>Ultimi Annunci</h2>
+        </div>
+        <div class="row">
+            @foreach ($announcements as $announcement)
+            <x-card :$announcement/>
+            @endforeach
+           
+        </div>
+    </div>
+    <hr class="">
     <x-categories />
 
 
