513c21c4ec1ff927dbf111b5061896ec2c34c88e
Simone Leonardi <simoneleo94@gmail.com>
2023-07-12 20:29:19 +0200

    merge

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <header >
   <div class="header-overlay">
     <h1>Presto.it</h1>
-    <p>{{__('ui.componetsHeader')}}</p>
+    <p  class="description_font">{{__('ui.componetsHeader')}}</p>
   </div>
 </header>
